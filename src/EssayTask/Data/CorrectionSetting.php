5a8d4ff6720818ea8383d7f34b4fc06b4c28a4dc
fneumann <fneumann@databay.de>
2025-01-31 02:11:16 +0100

    Rename CorrectorSettings to CorrectionSettings

--- src/EssayTask/Data/CorrectionSetting.php
+++ src/EssayTask/Data/CorrectionSetting.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Edutiek\AssessmentService\EssayTask\Data;
 
-abstract class CorrectorSetting implements EssayTaskEntity
+abstract class CorrectionSetting implements EssayTaskEntity
 {
     public abstract function getAssId(): int;
     public abstract function setAssId(int $ass_id): self;
