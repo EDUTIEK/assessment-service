05473de4c1a58d82e2ffc322a88e67f9e2751264
fneumann <neumann@ilias.de>
2025-11-10 00:46:08 +0100

    Convert inclusion setting to enablers

--- src/Task/Data/CorrectionSettings.php
+++ src/Task/Data/CorrectionSettings.php
@@ -14,4 +14,10 @@ abstract class CorrectionSettings implements TaskEntity
     abstract public function setPositiveRating(string $positive_rating): self;
     abstract public function getNegativeRating(): string;
     abstract public function setNegativeRating(string $negative_rating): self;
+    abstract public function getEnableComments(): bool;
+    abstract public function setEnableComments(bool $enable_comments): self;
+    abstract public function getEnableCommentRatings(): bool;
+    abstract public function setEnableCommentRatings(bool $enable_comment_ratings): self;
+    abstract public function getEnablePartialPoints(): bool;
+    abstract public function setEnablePartialPoints(bool $enable_partial_points): self;
 }
