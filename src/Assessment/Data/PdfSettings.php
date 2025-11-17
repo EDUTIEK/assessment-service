a2483599615982847051b5b1490b146f1b3e7e3e
fneumann <neumann@ilias.de>
2025-11-17 23:38:01 +0100

    Remove pdf margins from database

--- src/Assessment/Data/PdfSettings.php
+++ src/Assessment/Data/PdfSettings.php
@@ -25,24 +25,41 @@ abstract class PdfSettings implements AssessmentEntity, PdfCreatorSettings
 
     abstract public function getAssId(): int;
     abstract public function setAssId(int $ass_id): self;
-    abstract public function getAddHeader(): bool;
-    abstract public function setAddHeader(bool $add_header): self;
-    abstract public function getAddFooter(): bool;
-    abstract public function setAddFooter(bool $add_footer): self;
-    abstract public function getTopMargin(): int;
-    abstract public function setTopMargin(int $top_margin): self;
-    abstract public function getBottomMargin(): int;
-    abstract public function setBottomMargin(int $bottom_margin): self;
-    abstract public function getLeftMargin(): int;
-    abstract public function setLeftMargin(int $left_margin): self;
-    abstract public function getRightMargin(): int;
-    abstract public function setRightMargin(int $right_margin): self;
+
     abstract public function getFormat(): PdfFormat;
     abstract public function setFormat(PdfFormat $format): self;
 
     abstract public function getFeedbackMode(): PdfFeedbackMode;
     abstract public function setFeedbackMode(PdfFeedbackMode $mode): self;
 
+    public function getAddHeader(): bool
+    {
+        return true;
+    }
+
+    public function getAddFooter(): bool
+    {
+        return true;
+    }
+
+    public function getTopMargin(): int
+    {
+        return 10;
+    }
+
+    public function getBottomMargin(): int
+    {
+        return 10;
+    }
+
+    public function getLeftMargin(): int
+    {
+        return 10;
+    }
+    public function getRightMargin(): int
+    {
+        return 10;
+    }
 
     public function getHeaderMargin(): int
     {
