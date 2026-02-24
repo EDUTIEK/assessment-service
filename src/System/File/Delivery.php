34be38c6239e0972db30134beb43905d0e97387f
fneumann <fneumann@databay.de>
2026-02-24 09:40:46 +0100

    Allow cleanups after delivery

--- src/System/File/Delivery.php
+++ src/System/File/Delivery.php
@@ -16,11 +16,11 @@ interface Delivery
     /**
      * Send a file which is stored with the given id
      */
-    public function sendFile(string $id, Disposition $disposition, ?FileInfo $info = null): never;
+    public function sendFile(string $id, Disposition $disposition, ?FileInfo $info = null): void;
 
     /**
      * Send data content as a file with the given disposition
      * Use the file name and mime type provided by the file info
      */
-    public function sendData(string $data, Disposition $disposition, ?FileInfo $info): never;
+    public function sendData(string $data, Disposition $disposition, ?FileInfo $info): void;
 }
