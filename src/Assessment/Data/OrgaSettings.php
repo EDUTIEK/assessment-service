9859895d7ed201228e977f9030107ea4b9da1746
fneumann <fneumann@databay.de>
2026-08-12 12:09:49 +0200

    Add start password

--- src/Assessment/Data/OrgaSettings.php
+++ src/Assessment/Data/OrgaSettings.php
@@ -89,5 +89,8 @@ abstract class OrgaSettings implements AssessmentEntity
     abstract public function setSrcTemplateName(?string $name): self;
 
     abstract public function getDashboard(): bool;
-    abstract public function setDashboard(bool $dashboard): void;
+    abstract public function setDashboard(bool $dashboard): self;
+
+    abstract public function getStartPassword(): ?string;
+    abstract public function setStartPassword(?string $start_password): self;
 }
